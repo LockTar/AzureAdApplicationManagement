b3c578b34445dd69af68a818bb78397ea567918a
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-07 09:21:06 +0200

    Create set and remove

--- Vsts-Extension/Remove-AzureAdApplication/v1/Main.ps1
+++ Vsts-Extension/Remove-AzureAdApplication/v1/Main.ps1
@@ -4,7 +4,6 @@ Trace-VstsEnteringInvocation $MyInvocation
 $method = Get-VstsInput -Name method
 $objectId = Get-VstsInput -Name objectId
 $applicationId = Get-VstsInput -Name applicationId
-$name = Get-VstsInput -Name name
 
 # Initialize Azure Connection.
 Write-Verbose "Import module VstsAzureHelpers" 
@@ -15,29 +14,5 @@ Write-Verbose "Input variables are: "
 Write-Verbose "method: $method"
 Write-Verbose "objectId: $objectId"
 Write-Verbose "applicationId: $applicationId"
-Write-Verbose "name: $name"
 
-switch ($method)
-{
-    "objectid"
-    {
-        Write-Verbose "Get application by ObjectId"
-        
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ObjectId $objectId        
-    }
-    "applicationid"
-    {
-        Write-Verbose "Get application by ApplicationId"           
-
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ApplicationId $applicationId
-    }  
-    "name"
-    {
-        Write-Verbose "Get application by Name"
-
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ApplicationName $name
-    }
-    default{
-        Write-Error "Unknow method '$method'"
-    }
-}
+.\scripts\Remove-AzureAdApplication.ps1 -ObjectId $objectId -ApplicationId $applicationId
--- Vsts-Extension/Set-AzureAdApplication/v1/Main.ps1
+++ Vsts-Extension/Set-AzureAdApplication/v1/Main.ps1
@@ -1,10 +1,14 @@
 Trace-VstsEnteringInvocation $MyInvocation
 
 # Get inputs.
-$method = Get-VstsInput -Name method
-$objectId = Get-VstsInput -Name objectId
-$applicationId = Get-VstsInput -Name applicationId
-$name = Get-VstsInput -Name name
+$objectId = Get-VstsInput -Name objectId -Require
+$name = Get-VstsInput -Name name -Require
+$appIdUri = Get-VstsInput -Name appIdUri -Require
+$homePageUrl = Get-VstsInput -Name homePageUrl
+$logoutUrl = Get-VstsInput -Name logoutUrl
+$termsOfServiceUrl = Get-VstsInput -Name termsOfServiceUrl
+$privacyStatementUrl = Get-VstsInput -Name privacyStatementUrl
+$multiTenant = Get-VstsInput -Name multiTenant
 
 # Initialize Azure Connection.
 Write-Verbose "Import module VstsAzureHelpers" 
@@ -12,32 +16,21 @@ Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
 Initialize-Azure -strict
 
 Write-Verbose "Input variables are: "
-Write-Verbose "method: $method"
 Write-Verbose "objectId: $objectId"
-Write-Verbose "applicationId: $applicationId"
 Write-Verbose "name: $name"
-
-switch ($method)
-{
-    "objectid"
-    {
-        Write-Verbose "Get application by ObjectId"
+Write-Verbose "appIdUri: $appIdUri"
+Write-Verbose "homePageUrl: $homePageUrl"
+Write-Verbose "logoutUrl: $logoutUrl"
+Write-Verbose "termsOfServiceUrl: $termsOfServiceUrl"
+Write-Verbose "privacyStatementUrl: $privacyStatementUrl"
+Write-Verbose "multiTenant: $multiTenant"
         
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ObjectId $objectId        
-    }
-    "applicationid"
-    {
-        Write-Verbose "Get application by ApplicationId"           
-
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ApplicationId $applicationId
-    }  
-    "name"
-    {
-        Write-Verbose "Get application by Name"
-
-        .\scripts\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1 -ApplicationName $name
-    }
-    default{
-        Write-Error "Unknow method '$method'"
-    }
-}
+.\scripts\Set-AzureAdApplication.ps1 `
+    -ObjectId $objectId `
+    -Name $name `
+    -AppIdUri $appIdUri `
+    -HomePageUrl $homePageUrl `
+    -LogoutUrl $logoutUrl `
+    -TermsOfServiceUrl $termsOfServiceUrl `
+    -PrivacyStatementUrl $privacyStatementUrl `
+    -MultiTenant $multiTenant
