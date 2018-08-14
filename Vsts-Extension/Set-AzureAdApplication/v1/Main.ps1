614d1ba1885878a07a6c507c2ae5e8fa75e7fa24
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-14 18:05:59 +0200

    Add ability to change Required Resource Access

--- Vsts-Extension/Set-AzureAdApplication/v1/Main.ps1
+++ Vsts-Extension/Set-AzureAdApplication/v1/Main.ps1
@@ -35,6 +35,35 @@ Write-Verbose "multiTenant: $multiTenant"
 Write-Verbose "replyUrls: $replyUrls"
 Write-Verbose "replyUrlsArray: $replyUrlsArray"
 Write-Verbose "resourceAccessFilePath: $resourceAccessFilePath"
+
+Write-Verbose "Import AzureAD module because is not on default VSTS agent"
+$azureAdModulePath = $PSScriptRoot + "\AzureAD\2.0.1.16\AzureAD.psd1"
+Import-Module $azureAdModulePath 
+
+# Workaround to use AzureAD in this task. Get an access token and call Connect-AzureAD
+$serviceNameInput = Get-VstsInput -Name ConnectedServiceNameSelector -Require
+$serviceName = Get-VstsInput -Name $serviceNameInput -Require
+$endPointRM = Get-VstsEndpoint -Name $serviceName -Require
+
+$clientId = $endPointRM.Auth.Parameters.ServicePrincipalId
+$clientSecret = $endPointRM.Auth.Parameters.ServicePrincipalKey
+$tenantId = $endPointRM.Auth.Parameters.TenantId
+
+$adTokenUrl = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$resource = "https://graph.windows.net/"
+
+$body = @{
+    grant_type    = "client_credentials"
+    client_id     = $clientId
+    client_secret = $clientSecret
+    resource      = $resource
+}
+
+$response = Invoke-RestMethod -Method 'Post' -Uri $adTokenUrl -ContentType "application/x-www-form-urlencoded" -Body $body
+$token = $response.access_token
+
+Write-Verbose "Login to AzureAD with same application as endpoint"
+Connect-AzureAD -AadAccessToken $token -AccountId $clientId -TenantId $tenantId
         
 .\scripts\Set-AzureAdApplication.ps1 `
     -ObjectId $objectId `
