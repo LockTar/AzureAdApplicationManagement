7a4455482d9c63d895777893a9786c9823c0f075
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-21 10:23:31 +0200

    Copy readme via gulp because readme must be in root

--- Vsts-Extension/gulpfile.js
+++ Vsts-Extension/gulpfile.js
@@ -9,4 +9,11 @@ var hub = new HubRegistry(['gulp-tasks/*.js']);
 /* tell gulp to use the tasks just loaded */
 gulp.registry(hub);
 
-gulp.task('default', gulp.parallel('build'));
+
+function buildReadmeFile() {
+    console.log('Copy the readme file to the root of the extension');
+    return gulp.src('../Readme.md')
+      .pipe(gulp.dest('./'));
+}
+
+gulp.task('default', gulp.series(buildReadmeFile, gulp.parallel('build')));
