63f8ed138fa9f81e5fc0fff3452bbfa831641d3e
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-27 08:12:52 +0200

    Add option to let the build fail if the app cannot be found

--- Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV1/Main.ps1
+++ Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV1/Main.ps1
@@ -5,6 +5,7 @@ $method = Get-VstsInput -Name method
 $objectId = Get-VstsInput -Name objectId
 $applicationId = Get-VstsInput -Name applicationId
 $name = Get-VstsInput -Name name
+$failOnError = Get-VstsInput -Name failOnError -AsBool
 
 # Initialize Azure Connection.
 Write-Verbose "Import module VstsAzureHelpers" 
@@ -16,6 +17,7 @@ Write-Verbose "method: $method"
 Write-Verbose "objectId: $objectId"
 Write-Verbose "applicationId: $applicationId"
 Write-Verbose "name: $name"
+Write-Verbose "failOnError: $failOnError"
 
 switch ($method)
 {
@@ -23,19 +25,19 @@ switch ($method)
     {
         Write-Verbose "Get application by ObjectId"
         
-        .\scripts\Get-AzureAdApplication.ps1 -ObjectId $objectId        
+        .\scripts\Get-AzureAdApplication.ps1 -ObjectId $objectId -FailOnError $failOnError
     }
     "applicationid"
     {
         Write-Verbose "Get application by ApplicationId"           
 
-        .\scripts\Get-AzureAdApplication.ps1 -ApplicationId $applicationId
+        .\scripts\Get-AzureAdApplication.ps1 -ApplicationId $applicationId -FailOnError $failOnError
     }  
     "name"
     {
         Write-Verbose "Get application by Name"
 
-        .\scripts\Get-AzureAdApplication.ps1 -ApplicationName $name
+        .\scripts\Get-AzureAdApplication.ps1 -ApplicationName $name -FailOnError $failOnError
     }
     default{
         Write-Error "Unknow method '$method'"
