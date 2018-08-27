6974a3699eebd4eb7fe22ad62da3103e3f63b29d
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-27 07:48:15 +0200

    Continue if application can't be found

--- scripts/Get-AzureAdApplication/v1/Get-AzureAdApplication.ps1
+++ scripts/Get-AzureAdApplication/v1/Get-AzureAdApplication.ps1
@@ -4,7 +4,7 @@ Param(
     [string]$ApplicationName    
 )
 
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "SilentlyContinue"
 
 $oldverbose = $VerbosePreference
 $VerbosePreference = "continue"
@@ -24,19 +24,30 @@ else {
     Write-Error "At least one of the fields ObjectId, ApplicationId or ApplicationName must be given"
 }
 
-#Return application and his service principal
-$servicePrincipal = Get-AzureRmADServicePrincipal -ServicePrincipalName $application.ApplicationId
-#this doesn't work on vsts agent
-#$servicePrincipal = Get-AzureRmADApplication -ObjectId $application.ObjectId | Get-AzureRmADServicePrincipal
+if ($null -eq $application) {
+    Write-Host "##vso[task.setvariable variable=ObjectId;]"
+    Write-Host "##vso[task.setvariable variable=ApplicationId;]"
+    Write-Host "##vso[task.setvariable variable=Name;]"
+    Write-Host "##vso[task.setvariable variable=AppIdUri;]"
+    Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]"
+}
+else {
+    $ErrorActionPreference = "Stop"
 
-$application
-$servicePrincipal
+    # Return application and his service principal
+    $servicePrincipal = Get-AzureRmADServicePrincipal -ServicePrincipalName $application.ApplicationId
+    #this doesn't work on vsts agent
+    #$servicePrincipal = Get-AzureRmADApplication -ObjectId $application.ObjectId | Get-AzureRmADServicePrincipal
 
-Write-Host "##vso[task.setvariable variable=ObjectId;]$($application.ObjectId)"
-Write-Host "##vso[task.setvariable variable=ApplicationId;]$($application.ApplicationId)"
-Write-Host "##vso[task.setvariable variable=Name;]$($application.DisplayName)"
-Write-Host "##vso[task.setvariable variable=AppIdUri;]$($application.IdentifierUris[0])"
-Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]$($servicePrincipal.Id)"
+    $application
+    $servicePrincipal
+
+    Write-Host "##vso[task.setvariable variable=ObjectId;]$($application.ObjectId)"
+    Write-Host "##vso[task.setvariable variable=ApplicationId;]$($application.ApplicationId)"
+    Write-Host "##vso[task.setvariable variable=Name;]$($application.DisplayName)"
+    Write-Host "##vso[task.setvariable variable=AppIdUri;]$($application.IdentifierUris[0])"
+    Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]$($servicePrincipal.Id)"
+}
 
 $VerbosePreference = $oldverbose
 $InformationPreference = $oldinformation
