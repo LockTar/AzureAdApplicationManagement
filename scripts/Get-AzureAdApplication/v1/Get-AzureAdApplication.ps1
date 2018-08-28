1ea649d491096ea1417e8efe36f4952538f412bc
Ralph Jansen <ralph.jansen@outlook.com>
2018-08-28 17:03:19 +0200

    Also return the homepageurl by new and get. also make required by set

--- scripts/Get-AzureAdApplication/v1/Get-AzureAdApplication.ps1
+++ scripts/Get-AzureAdApplication/v1/Get-AzureAdApplication.ps1
@@ -44,6 +44,7 @@ if ($null -eq $application) {
         Write-Host "##vso[task.setvariable variable=ApplicationId;]"
         Write-Host "##vso[task.setvariable variable=Name;]"
         Write-Host "##vso[task.setvariable variable=AppIdUri;]"
+        Write-Host "##vso[task.setvariable variable=HomePageUrl;]"
         Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]"
     }
 }
@@ -65,6 +66,7 @@ else {
     Write-Host "##vso[task.setvariable variable=ApplicationId;]$($application.ApplicationId)"
     Write-Host "##vso[task.setvariable variable=Name;]$($application.DisplayName)"
     Write-Host "##vso[task.setvariable variable=AppIdUri;]$($application.IdentifierUris[0])"
+    Write-Host "##vso[task.setvariable variable=HomePageUrl;]$($application.HomePage)"
     Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]$($servicePrincipal.Id)"
 }
 
