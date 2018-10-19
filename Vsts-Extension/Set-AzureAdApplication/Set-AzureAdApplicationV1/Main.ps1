30752dd423a4e4ce2e634136bc491f6d611e6287
Ralph Jansen <ralph.jansen@outlook.com>
2018-10-19 15:08:58 +0200

    Use write verbose otherwise we won't see it in vsts

--- Vsts-Extension/Set-AzureAdApplication/Set-AzureAdApplicationV1/Main.ps1
+++ Vsts-Extension/Set-AzureAdApplication/Set-AzureAdApplicationV1/Main.ps1
@@ -27,27 +27,27 @@ if ($owners -ne "") {
 }
 
 # Initialize Azure Connection.
-Write-Debug "Import module VstsAzureHelpers" 
+Write-Verbose "Import module VstsAzureHelpers" 
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
 Initialize-Azure -strict
 
-Write-Debug "Input variables are: "
-Write-Debug "createIfNotExist: $createIfNotExist"
-Write-Debug "objectId: $objectId"
-Write-Debug "name: $name"
-Write-Debug "appIdUri: $appIdUri"
-Write-Debug "homePageUrl: $homePageUrl"
-Write-Debug "logoutUrl: $logoutUrl"
-Write-Debug "termsOfServiceUrl: $termsOfServiceUrl"
-Write-Debug "privacyStatementUrl: $privacyStatementUrl"
-Write-Debug "multiTenant: $multiTenant"
-Write-Debug "replyUrls: $replyUrls"
-Write-Debug "replyUrlsArray: $replyUrlsArray"
-Write-Debug "resourceAccessFilePath: $resourceAccessFilePath"
-Write-Debug "owners: $owners"
-Write-Debug "ownersArray: $ownersArray"
+Write-Verbose "Input variables are: "
+Write-Verbose "createIfNotExist: $createIfNotExist"
+Write-Verbose "objectId: $objectId"
+Write-Verbose "name: $name"
+Write-Verbose "appIdUri: $appIdUri"
+Write-Verbose "homePageUrl: $homePageUrl"
+Write-Verbose "logoutUrl: $logoutUrl"
+Write-Verbose "termsOfServiceUrl: $termsOfServiceUrl"
+Write-Verbose "privacyStatementUrl: $privacyStatementUrl"
+Write-Verbose "multiTenant: $multiTenant"
+Write-Verbose "replyUrls: $replyUrls"
+Write-Verbose "replyUrlsArray: $replyUrlsArray"
+Write-Verbose "resourceAccessFilePath: $resourceAccessFilePath"
+Write-Verbose "owners: $owners"
+Write-Verbose "ownersArray: $ownersArray"
 
-Write-Debug "Import AzureAD module because is not on default VSTS agent"
+Write-Verbose "Import AzureAD module because is not on default VSTS agent"
 $azureAdModulePath = $PSScriptRoot + "\AzureAD\2.0.1.16\AzureAD.psd1"
 Import-Module $azureAdModulePath 
 
@@ -73,29 +73,29 @@ $body = @{
 $response = Invoke-RestMethod -Method 'Post' -Uri $adTokenUrl -ContentType "application/x-www-form-urlencoded" -Body $body
 $token = $response.access_token
 
-Write-Debug "Login to AzureAD with same application as endpoint"
+Write-Verbose "Login to AzureAD with same application as endpoint"
 Connect-AzureAD -AadAccessToken $token -AccountId $clientId -TenantId $tenantId
 
-Write-Debug "Add service principal of the azurerm connection to the array of owners"
+Write-Verbose "Add service principal of the azurerm connection to the array of owners"
 $deployServicePrincipalId = (Get-AzureRmADServicePrincipal -ServicePrincipalName $clientId).Id
 $ownersArray += $deployServicePrincipalId
 
 if ($createIfNotExist) {
-    Write-Debug "Check if the application '$name' exists"
+    Write-Verbose "Check if the application '$name' exists"
     $result = .\scripts\Get-AzureAdApplication.ps1 -ApplicationName $name -FailOnError $false
 
     if (!$result.Application) {
-        Write-Debug "Application doesn't exist. Create the application '$name'"
+        Write-Verbose "Application doesn't exist. Create the application '$name'"
         .\scripts\New-AzureAdApplication.ps1 `
             -ApplicationName $name `
             -SignOnUrl $homePageUrl
 
         $secondsToWait = 60
-        Write-Debug "Application '$name' is created but wait $secondsToWait seconds so Azure AD can process it and we can set all the properties"
+        Write-Verbose "Application '$name' is created but wait $secondsToWait seconds so Azure AD can process it and we can set all the properties"
         Start-Sleep -Seconds $secondsToWait
     }
 
-    Write-Debug "Get the application '$name' again so we have the ObjectId to alter the application"
+    Write-Verbose "Get the application '$name' again so we have the ObjectId to alter the application"
     $result = .\scripts\Get-AzureAdApplication.ps1 -ApplicationName $name -FailOnError $false
 
     $objectId = $result.Application.ObjectId
