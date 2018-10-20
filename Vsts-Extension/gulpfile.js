4e90f4ecd544459fcc9d0628a38ef5d4aebf0719
Ralph Jansen <ralph.jansen@outlook.com>
2018-10-20 12:34:21 +0200

    Move gulp tasks to v1 folder so we can create v2 folder

--- Vsts-Extension/gulpfile.js
+++ Vsts-Extension/gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var HubRegistry = require('gulp-hub');
 
 /* load some files into the registry */
-var hub = new HubRegistry(['gulp-tasks/*.js']);
+var hub = new HubRegistry(['gulp-tasks/**/*.js']);
 
 /* tell gulp to use the tasks just loaded */
 gulp.registry(hub);
