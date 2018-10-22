fe25ce142e93e128be1c51c4f23a6c4ee9499bf7
Ralph Jansen <ralph.jansen@outlook.com>
2018-10-22 07:33:19 +0200

    Completely change new login method. Let's test on agent

--- Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV2/Main.ps1
+++ Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV2/Main.ps1
@@ -7,10 +7,31 @@ $applicationId = Get-VstsInput -Name applicationId
 $name = Get-VstsInput -Name name
 $failIfNotFound = Get-VstsInput -Name failIfNotFound -AsBool
 
+Import-Module $PSScriptRoot\AzureRM\AzureRM.profile\5.6.0\AzureRM.Profile.psd1
+Import-Module $PSScriptRoot\AzureRM\AzureRM.Resources\6.6.0\AzureRM.Resources.psd1
+
 # Initialize Azure Connection.
-Write-Verbose "Import module VstsAzureHelpers" 
-Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
-Initialize-Azure -strict
+#Write-Verbose "Import module VstsAzureHelpers" 
+#Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
+#Initialize-Azure -strict
+
+$serviceNameInput = Get-VstsInput -Name ConnectedServiceNameSelector -Require
+$serviceName = Get-VstsInput -Name $serviceNameInput -Require
+$endPointRM = Get-VstsEndpoint -Name $serviceName -Require
+
+$endPointRM = Get-VstsEndpoint -Name $serviceName -Require
+$clientId = $endPointRM.Auth.Parameters.ServicePrincipalId
+$clientSecret = $endPointRM.Auth.Parameters.ServicePrincipalKey
+$tenantId = $endPointRM.Auth.Parameters.TenantId
+$environmentName = "AzureCloud"
+$subscriptionId = $Endpoint.Data.SubscriptionId
+
+$psCredential = New-Object System.Management.Automation.PSCredential(
+    $clientId,
+    (ConvertTo-SecureString $clientSecret -AsPlainText -Force))
+
+$null = Connect-AzureRMAccount -ServicePrincipal -Tenant $tenantId -Credential $psCredential -Environment $environmentName
+$null = Set-AzureRmContext -SubscriptionId $subscriptionId -Tenant $tenantId
 
 Write-Verbose "Input variables are: "
 Write-Verbose "method: $method"
