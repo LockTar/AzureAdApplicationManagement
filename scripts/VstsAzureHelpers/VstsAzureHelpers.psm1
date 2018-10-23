5f996b0edfac31514531cc6e6ea6cdcff9aaee57
Ralph Jansen <ralph.jansen@outlook.com>
2018-10-23 20:52:38 +0200

    Update module

--- scripts/VstsAzureHelpers/VstsAzureHelpers.psm1
+++ scripts/VstsAzureHelpers/VstsAzureHelpers.psm1
@@ -28,5 +28,57 @@ function Initialize-Azure {
     }
 }
 
+function Initialize-PackageProvider {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$Name = "NuGet"
+    )
+
+    Write-Verbose "Initialize package provider $Name"
+    $packageProvider = Get-PackageProvider -Name $Name -ListAvailable
+    
+    if($packageProvider)
+    {
+        Write-Verbose "Package provider $Name with version $($packageProvider.Version) already installed"
+    }
+    else
+    {     
+        Write-Verbose "Install package provider $Name"
+        Find-PackageProvider -Name $Name | Install-PackageProvider -Verbose -Scope CurrentUser -Force
+    }
+}
+
+function Initialize-Package {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Name,
+        [Parameter()]
+        [string]$RequiredVersion
+    )
+
+    Write-Verbose "Initialize package $Name"
+    $package = Get-Package -Name $Name -RequiredVersion $RequiredVersion -ErrorAction SilentlyContinue
+
+    if($package)
+    {
+        Write-Verbose "Package $Name with version $($package.Version) already installed"
+    }
+    else
+    {        
+        if($RequiredVersion)
+        {
+            Write-Verbose "Install package $Name with version $RequiredVersion"
+            Find-Package $Name -RequiredVersion $RequiredVersion | Install-Package -Scope CurrentUser -Force
+        }
+        else
+        {
+            Write-Verbose "Install package $Name"
+            Find-Package $Name | Install-Package -Scope CurrentUser -Force
+        }
+    }
+}
+
 # Export only the public function.
-Export-ModuleMember -Function Initialize-Azure
+Export-ModuleMember -Function Initialize-Azure, Initialize-PackageProvider, Initialize-Package
