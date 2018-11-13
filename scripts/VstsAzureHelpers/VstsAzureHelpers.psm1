5231005756b884cee9d93328b76ceb7c97357124
Ralph Jansen <ralph.jansen@outlook.com>
2018-11-13 17:57:03 +0100

    Use AzureRM version on disk if available

--- scripts/VstsAzureHelpers/VstsAzureHelpers.psm1
+++ scripts/VstsAzureHelpers/VstsAzureHelpers.psm1
@@ -83,39 +83,21 @@ function Initialize-Module {
         New-Item -Path $modulePath -ItemType Directory
     }
 
-    # Check if AzureRM module is locally available via an already downloaded version on the system drive (hosted microsoft agents)
-    $targetAzureRmVersion = Get-AzureRMVersion -AzureRMModuleName $Name -RequiredVersion $RequiredVersion
-    $installedAzureRmVersions = "2.1.0","3.8.0","4.2.1","5.1.1","6.7.0"
-    $hostedAgentAzureRMDownloadPath = ('c:\Modules\AzureRM_{0}' -f $targetAzureRmVersion)
-        
-    if ($installedAzureRmVersions.Contains($targetAzureRmVersion) -and (Test-Path -Path $hostedAgentAzureRMDownloadPath)) {
-        Write-Verbose -Message ('Module {0} with Version {1} is locally available in AzureRM version {2}' -f $Name, $RequiredVersion, $targetAzureRmVersion)
-        Write-Verbose "Add local AzureRM PowerShell modules path to the PSModulePath Environment variable"
-        $modulePath = $hostedAgentAzureRMDownloadPath
-        if (!$env:PSModulePath.Contains($modulePath)) {
-            $env:PSModulePath = $modulePath + ';' + $env:PSModulePath
-        }
+    $moduleSetOnDisk = $false
 
-        Write-Verbose "Check if Module with correct version $RequiredVersion is available on system"
-        $module = Get-Module -Name $Name -ListAvailable | Where-Object {$_.Version -eq $RequiredVersion -and $_.Name -eq $_.Name}
-        if (!($module)) {
-            Write-Error -Message ('Module {0} with Version {1} is NOT locally available' -f $Name, $RequiredVersion)
-        }
-        else {
-            # List all the locations where the Module version is located.
-            $module | ForEach-Object {
-                Write-Verbose -Message ('Module {0} with Version {1} is locally available in folder {2}' -f $Name, $RequiredVersion, $_.ModuleBase)
-            }
-        }
+    if($Name.StartsWith("AzureRM")){
+        $versionSetOnDisk = Set-AzureRmVersionOnDiskInModulePath -Name $Name -RequiredVersion $RequiredVersion
+        $moduleSetOnDisk = $versionSetOnDisk
     }
-    else {
+
+    if (!$moduleSetOnDisk) {
         Write-Verbose "Add custom PowerShell modules path to the PSModulePath Environment variable"
         if (!$env:PSModulePath.Contains($modulePath)) {
             $env:PSModulePath = $modulePath + ';' + $env:PSModulePath
         }
     
         Write-Verbose -Message  ('Check if Module {0} with correct version {1} is available on system' -f $Name, $RequiredVersion)
-        $module = Get-Module -Name $Name -ListAvailable | Where-Object {$_.Version -eq $RequiredVersion} #Rema
+        $module = Get-Module -Name $Name -ListAvailable | Where-Object {$_.Version -eq $RequiredVersion} 
 
         if ($module) {
             Write-Verbose ('Module {0} with version {1} already installed' -f $module.Name, $module.Version)
@@ -133,59 +115,5 @@ function Initialize-Module {
     }
 }
 
-<#
-.Synopsis
-   Retrieve AzureRM PowerShell version containing AzureRM Powershell module.
-.DESCRIPTION
-   Retrieve AzureRM PowerShell version containing AzureRM Powershell module with required version from PowerShell Gallery.
-.EXAMPLE
-   Get-AzureRMVersion -AzureRMModuleName 'AzureRM.ApplicationInsights' -RequiredVersion '0.1.8'
-   6.11.0
-.INPUTS
-   AzureRMModuleName. AzureRM PowerShell Module Name.
-.INPUTS
-   RequiredVersion. AzureRM PowerShell Module Version.
-.OUTPUTS
-   AzureRM (Meta) PowerShell Module Version.
-#>
-function Get-AzureRMVersion {
-    [CmdletBinding()]
-    [OutputType([string])]
-    Param
-    (
-        [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $true,
-            Position = 0)]
-        [string]$AzureRMModuleName,
-
-        [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $true,
-            Position = 1)]
-        [string]
-        $RequiredVersion
-    )
-
-    #region check AzureRm PowerShell module version for specific Module.
-    Write-Verbose -Message ('Retrieve all AzureRM module versions from PSGallery')
-    $AllAzureRMModules = Find-Module -Name AzureRM -AllVersions
-    Write-Verbose -Message ('{0} AzureRM Module versions retrieve from the PSGallery' -f $AllAzureRMModules.count)
-    #endregion
-
-    #region get AzureRM PowerShell module version container the specified AzureRM.[name] Module version
-    Write-Verbose -Message ('Find Module {0} with version {1} in AzureRM Module versions' -f $($AzureRMModuleName), $($RequiredVersion))
-    foreach ($AzureRMModule in $AllAzureRMModules) {
-        Write-Verbose -Message ('Checking AzureRM Module version {0} for Module {1} with version {2}' -f $($AzureRMModule.Version), $($AzureRMModuleName), $($RequiredVersion))
-        if ($AzureRMModule.dependencies | Where-Object {($_.Name -eq $($AzureRMModuleName) -and $_.RequiredVersion -eq $RequiredVersion) }) {
-            $AzureRMModuleVersion = $AzureRMModule.Version
-            Write-Verbose -Message ('Found AzureRM Module with version {0} for Module {1} with version {2}' -f $($AzureRMModule.Version), $($AzureRMModuleName), $($RequiredVersion))
-            return $AzureRMModuleVersion
-        }
-    }
-    if (!($AzureRMModuleVersion)) {
-        Write-Error -Message ('No AzureRM Module version found which contains Module {0} with version {1}' -f $($AzureRMModuleName), $($RequiredVersion))
-    }
-    #endregion
-}
-
 # Export only the public function.
-Export-ModuleMember -Function Initialize-AzureRM, Initialize-AzureAD, Initialize-PackageProvider, Initialize-Module, Get-AzureRMVersion
+Export-ModuleMember -Function Initialize-AzureRM, Initialize-AzureAD, Initialize-PackageProvider, Initialize-Module
