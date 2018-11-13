1cf60e78292ee9888d7ecd4c6298ad778b4cee94
Ralph Jansen <ralph.jansen@outlook.com>
2018-11-13 19:13:12 +0100

    test with 6.4.0 because is on disk on hosted agent

--- Vsts-Extension/Set-AzureAdApplication/Set-AzureAdApplicationV2/Main.ps1
+++ Vsts-Extension/Set-AzureAdApplication/Set-AzureAdApplicationV2/Main.ps1
@@ -43,7 +43,7 @@ if ($owners -ne "") {
 # Initialize Azure Connection
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers\VstsAzureHelpers.psm1
 Initialize-PackageProvider
-Initialize-Module -Name "AzureRM.Resources" -RequiredVersion "6.7.0"
+Initialize-Module -Name "AzureRM.Resources" -RequiredVersion "6.4.0"
 Initialize-AzureRM
 
 Initialize-Module -Name "AzureAD" -RequiredVersion "2.0.2.4"
