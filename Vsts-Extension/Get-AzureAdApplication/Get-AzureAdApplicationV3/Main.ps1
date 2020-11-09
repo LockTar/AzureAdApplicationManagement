bf03aa1dec14b904983aa0f1312db95e30b86d8c
LockTar <ralph.jansen@outlook.com>
2020-11-09 15:04:57 +0100

    Convert to the new way with Az Module

--- Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV3/Main.ps1
+++ Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationV3/Main.ps1
@@ -8,40 +8,103 @@ $name = Get-VstsInput -Name name
 $failIfNotFound = Get-VstsInput -Name failIfNotFound -AsBool
 
 # Initialize Azure Connection
-Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers\VstsAzureHelpers.psm1
-Initialize-PackageProvider
-Initialize-Module -Name "AzureRM.Resources" -RequiredVersion "6.7.0"
-Initialize-AzureRM
+#Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers\VstsAzureHelpers.psm1
+#Initialize-PackageProvider
+#Initialize-Module -Name "Az" -RequiredVersion "5.0.0"
+#Initialize-AzureAz
 
-Write-Verbose "Input variables are: "
-Write-Verbose "method: $method"
-Write-Verbose "objectId: $objectId"
-Write-Verbose "applicationId: $applicationId"
-Write-Verbose "name: $name"
-Write-Verbose "failIfNotFound: $failIfNotFound"
+# Mock values like you would get from task.json like https://github.com/microsoft/azure-pipelines-tasks/blob/master/Tasks/AzurePowerShellV5/AzurePowerShell.ps1
+$targetAzurePs = "OtherVersion" 
+$customTargetAzurePs = "5.0.0" # This is the version of the Azure Az PowerShell module
 
-Import-Module $PSScriptRoot\scripts\Get-AadApplication.psm1
+# string constants
+$otherVersion = "OtherVersion"
+$latestVersion = "LatestVersion"
 
-switch ($method)
-{
-    "objectid"
-    {
-        Write-Verbose "Get application by ObjectId"        
-        $null = Get-AadApplication -ObjectId $objectId -FailIfNotFound $failIfNotFound
-    }
-    "applicationid"
-    {
-        Write-Verbose "Get application by ApplicationId"
-        $null = Get-AadApplication -ApplicationId $applicationId -FailIfNotFound $failIfNotFound
-    }  
-    "name"
-    {
-        Write-Verbose "Get application by Name"
-        $null = Get-AadApplication -ApplicationName $name -FailIfNotFound $failIfNotFound
-    }
-    default{
-        Write-Error "Unknow method '$method'"
+if ($targetAzurePs -eq $otherVersion) {
+    if ($customTargetAzurePs -eq $null) {
+        throw (Get-VstsLocString -Key InvalidAzurePsVersion $customTargetAzurePs)
+    } else {
+        $targetAzurePs = $customTargetAzurePs.Trim()
     }
 }
 
-Trace-VstsLeavingInvocation $MyInvocation
+$pattern = "^[0-9]+\.[0-9]+\.[0-9]+$"
+$regex = New-Object -TypeName System.Text.RegularExpressions.Regex -ArgumentList $pattern
+
+if ($targetAzurePs -eq $latestVersion) {
+    $targetAzurePs = ""
+} elseif (-not($regex.IsMatch($targetAzurePs))) {
+    throw (Get-VstsLocString -Key InvalidAzurePsVersion -ArgumentList $targetAzurePs)
+}
+
+. "$PSScriptRoot\Utility.ps1"
+
+$serviceName = Get-VstsInput -Name ConnectedServiceNameARM -Require
+$endpointObject = Get-VstsEndpoint -Name $serviceName -Require
+$endpoint = ConvertTo-Json $endpointObject
+
+try 
+{
+    # # Generate the script contents.
+    # Write-Host (Get-VstsLocString -Key 'GeneratingScript')
+    # $contents = @()
+    # $contents += "`$ErrorActionPreference = '$__vsts_input_errorActionPreference'"
+    # if ($env:system_debug -eq "true") {
+    #     $contents += "`$VerbosePreference = 'continue'"
+    # }
+
+    $CoreAzArgument = $null;
+    if ($targetAzurePs) {
+        $CoreAzArgument = "-endpoint '$endpoint' -targetAzurePs $targetAzurePs"
+    } else {
+        $CoreAzArgument = "-endpoint '$endpoint'"
+    }
+    #$contents += ". $PSScriptRoot\CoreAz.ps1 $CoreAzArgument"
+    . $PSScriptRoot\CoreAz.ps1 $CoreAzArgument
+
+
+
+
+
+    
+    Write-Verbose "Input variables are: "
+    Write-Verbose "method: $method"
+    Write-Verbose "objectId: $objectId"
+    Write-Verbose "applicationId: $applicationId"
+    Write-Verbose "name: $name"
+    Write-Verbose "failIfNotFound: $failIfNotFound"
+
+    Import-Module $PSScriptRoot\scripts\Get-AadApplication.psm1
+
+    switch ($method)
+    {
+        "objectid"
+        {
+            Write-Verbose "Get application by ObjectId"        
+            $null = Get-AadApplication -ObjectId $objectId -FailIfNotFound $failIfNotFound
+        }
+        "applicationid"
+        {
+            Write-Verbose "Get application by ApplicationId"
+            $null = Get-AadApplication -ApplicationId $applicationId -FailIfNotFound $failIfNotFound
+        }  
+        "name"
+        {
+            Write-Verbose "Get application by Name"
+            $null = Get-AadApplication -ApplicationName $name -FailIfNotFound $failIfNotFound
+        }
+        default {
+            Write-Error "Unknow method '$method'"
+        }
+    }
+}
+finally {
+    if ($__vstsAzPSInlineScriptPath -and (Test-Path -LiteralPath $__vstsAzPSInlineScriptPath) ) {
+        Remove-Item -LiteralPath $__vstsAzPSInlineScriptPath -ErrorAction 'SilentlyContinue'
+    }
+
+    Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
+    Remove-EndpointSecrets
+    Disconnect-AzureAndClearContext -ErrorAction SilentlyContinue
+}
