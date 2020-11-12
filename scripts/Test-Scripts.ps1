aee18512f1a5d5512a0ff0552c00e931616c3796
LockTar <ralph.jansen@outlook.com>
2020-11-12 14:43:24 +0100

    Update script for now like this

--- scripts/Test-Scripts.ps1
+++ scripts/Test-Scripts.ps1
@@ -5,21 +5,34 @@ $VerbosePreference = "continue"
 $oldinformation = $InformationPreference
 $InformationPreference = "continue"
 
-$ApplicationName = "TestApplicationExtension"
+$ApplicationName = "ApplicationManagementExtensionV3"
+
+Get-Module Get-AadApplication | Remove-Module
+Get-Module New-AadApplication | Remove-Module
+Get-Module Set-AadApplication | Remove-Module
+Get-Module Remove-AadApplication | Remove-Module
+
+Import-Module $PSScriptRoot\Get-AzureAdApplication\v3\Get-AadApplication
+Import-Module $PSScriptRoot\New-AzureAdApplication\v3\New-AadApplication.psm1
+Import-Module $PSScriptRoot\Set-AzureAdApplication\v3\Set-AadApplication.psm1
+Import-Module $PSScriptRoot\Remove-AzureAdApplication\v3\Remove-AadApplication.psm1
 
 try {
-    & $PSScriptRoot"\Get-AzureAdApplication\v1\Get-AzureAdApplication.ps1" -ApplicationName $ApplicationName -FailIfNotFound $True
+    Get-AadApplication -ApplicationName $ApplicationName -FailIfNotFound $true
 }
 catch {
     Write-Information "Application doesn't exist, let's create one"
-    & $PSScriptRoot"\New-AzureAdApplication\v1\New-AzureAdApplication.ps1" -ApplicationName $ApplicationName -SignOnUrl "https://$ApplicationName.com"# -AppIdUri "http://ralphjansenoutlook.onmicrosoft.com/$ApplicationName"
+    New-AadApplication -ApplicationName $ApplicationName -HomePageUrl "https://$ApplicationName.com"# -AppIdUri "http://ralphjansenoutlook.onmicrosoft.com/$ApplicationName"
     Start-Sleep -Seconds 15
 }
 
-$application = Get-AzureRmADApplication -DisplayName $ApplicationName
+$application = Get-AadApplication -ApplicationName $ApplicationName
+$application = $application.Application
 
 try {
-    #& $PSScriptRoot"\Set-AzureAdApplication\v1\Set-AzureAdApplication.ps1" `
+    Get-AadApplication -ApplicationName $ApplicationName
+    
+    # Set-AadApplication `
     #    -ObjectId $application.ObjectId `
     #    -Name "$($ApplicationName)2" `
     #    -AppIdUri "https://6e93a626-8aca-4dc1-9191-ce291b4b75a1/$($ApplicationName)2" `
@@ -30,10 +43,10 @@ try {
     #    -MultiTenant $False `
     #    -ResourceAccessFilePath $PSScriptRoot"\Test-RequiredResourceAccess.json" `
     #    -Owners @("bf41f70e-be3c-473a-b594-1e7c57b28da4", "2e5cb30e-7ce4-412f-82b9-70998ec8ac7d")
-#
+
     #Start-Sleep -Seconds 15
-#
-    #& $PSScriptRoot"\Set-AzureAdApplication\v1\Set-AzureAdApplication.ps1" `
+
+    # Set-AadApplication `
     #    -ObjectId $application.ObjectId `
     #    -Name $ApplicationName `
     #    -AppIdUri "https://6e93a626-8aca-4dc1-9191-ce291b4b75a1/$ApplicationName" `
@@ -47,12 +60,12 @@ try {
 #
     #Start-Sleep -Seconds 15
 
-    & $PSScriptRoot"\Remove-AzureAdApplication\v1\Remove-AzureAdApplication.ps1" -ObjectId $application.ObjectId
+    Remove-AadApplication -ObjectId $application.ObjectId
 }
 catch {
-    Write-Verbose "Set failed so remove the application to clean up"
-    & $PSScriptRoot"\Remove-AzureAdApplication\v1\Remove-AzureAdApplication.ps1" -ObjectId $application.ObjectId
-    throw
+    Write-Information "Script failed so remove the application so we can start fresh next time"
+    $application
+    Remove-AadApplication -ObjectId $application.ObjectId
 }
 
 $VerbosePreference = $oldverbose
