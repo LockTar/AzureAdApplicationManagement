70b5bd32b223726c480591868c5bc605f1d3e13e
LockTar <ralph.jansen@outlook.com>
2020-11-18 21:17:58 +0100

    Create tests for new task

--- scripts/ManageAadApplications/v3/New-AadApplication.Tests.ps1
+++ scripts/ManageAadApplications/v3/New-AadApplication.Tests.ps1
@@ -4,7 +4,7 @@ BeforeAll {
 }
 
 Describe 'New-AadApplication' {
-    Context "No parameter names" {
+    Context "New-AadApplication" {
         It "Given a name should return application and service principal with default identifieruri" {
             $result = New-AadApplication "AzureAdApplicationManagementTestApp1"
             
@@ -13,6 +13,43 @@ Describe 'New-AadApplication' {
             $result.ServicePrincipal | Should -BeNullOrEmpty -Not
             $result.Application.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
             $result.Application.IdentifierUris[0] | Should -Be "api://$($result.Application.ApplicationId)"
+            $result.ServicePrincipal.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+        }
+        
+        It "Given an empty name should fail" {
+            { New-AadApplication -DisplayName "" } | Should -Throw
+        }
+
+        It "Given a name should return application and service principal with default identifieruri" {
+            $result = New-AadApplication -DisplayName "AzureAdApplicationManagementTestApp1"
+            
+            $result | Should -BeNullOrEmpty -Not
+            $result.Application | Should -BeNullOrEmpty -Not
+            $result.ServicePrincipal | Should -BeNullOrEmpty -Not
+            $result.Application.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+            $result.Application.IdentifierUris[0] | Should -Be "api://$($result.Application.ApplicationId)"
+            $result.ServicePrincipal.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+        }
+
+        It "Given an empty identifieruri should return application and service principal with default identifieruri" {
+            $result = New-AadApplication -DisplayName "AzureAdApplicationManagementTestApp1" -IdentifierUri ""
+            
+            $result | Should -BeNullOrEmpty -Not
+            $result.Application | Should -BeNullOrEmpty -Not
+            $result.ServicePrincipal | Should -BeNullOrEmpty -Not
+            $result.Application.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+            $result.Application.IdentifierUris[0] | Should -Be "api://$($result.Application.ApplicationId)"
+            $result.ServicePrincipal.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+        }
+
+        It "Given an identifieruri should return application and service principal with given identifieruri" {
+            $result = New-AadApplication -DisplayName "AzureAdApplicationManagementTestApp1" -IdentifierUri "https://SampleIdentifier"
+            
+            $result | Should -BeNullOrEmpty -Not
+            $result.Application | Should -BeNullOrEmpty -Not
+            $result.ServicePrincipal | Should -BeNullOrEmpty -Not
+            $result.Application.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
+            $result.Application.IdentifierUris[0] | Should -Be "https://SampleIdentifier"
             $result.ServicePrincipal.DisplayName | Should -Be "AzureAdApplicationManagementTestApp1"
         }
         
