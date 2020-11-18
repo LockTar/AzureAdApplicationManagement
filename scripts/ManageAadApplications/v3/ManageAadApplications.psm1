2f23e4db3a5590a35c87923f42321e5465f4a9ea
LockTar <ralph.jansen@outlook.com>
2020-11-18 20:58:31 +0100

    Start with new cmdlet

--- scripts/ManageAadApplications/v3/ManageAadApplications.psm1
+++ scripts/ManageAadApplications/v3/ManageAadApplications.psm1
@@ -1,18 +1,57 @@
 
 function New-AadApplication {
 
-    [CmdletBinding(SupportsShouldProcess)]
+    [CmdletBinding(DefaultParameterSetName = "DisplayName")]
     param (
         [ValidateNotNullOrEmpty()]
-        [string]$DisplayName
+        [Parameter(Position = 0, ParameterSetName = "DisplayName", Mandatory = $true)]
+        [string]$DisplayName,
+        [string]$IdentifierUri
     )
 
-    Write-Verbose -Message "Create new application $DisplayName"
+    Write-Verbose "Create new application $DisplayName"
         
-    $app = New-AzADApplication -DisplayName $DisplayName -IdentifierUris @("api://$DisplayName", "https://$DisplayName")
+    $identifierUriFromParameter = $false
+    Write-Verbose "Check if IdentifierUri is given as parameter"
+    if ([string]::IsNullOrWhiteSpace($IdentifierUri)) {
+        Write-Verbose "No IdentifierUri so generate one with format: https://{DisplayName}"        
+        $IdentifierUri = "https://$DisplayName"
+        $identifierUriFromParameter = $false
+        Write-Verbose "Generated IdentifierUri: $IdentifierUri"
+    }
+    else {
+        Write-Verbose "Use given IdentifierUri: $IdentifierUri"
+        $identifierUriFromParameter = $true
+    }
 
-    Write-Information "Created application with name $DisplayName under ObjectId $($app.ObjectId) and ApplicationId $($app.ApplicationId)"
-    $app
+    Write-Verbose "Create application $DisplayName"
+    $app = New-AzADApplication -DisplayName $DisplayName -IdentifierUris $IdentifierUri
+    
+    if ($identifierUriFromParameter -eq $false) {
+        # The IdentifierUri was not given as parameter so create the new default identifieruri. This can only be done with the ApplicationId so use it from the 'New' action.
+        Write-Verbose "Change IdentifierUri to the new default format of Microsoft: api://{ApplicationId}"
+        $IdentifierUri = "api://$($app.ApplicationId)"
+        $app = Update-AzADApplication -ObjectId $app.ObjectId -IdentifierUris $IdentifierUri
+    }
+
+    Write-Verbose "Create service principal connected to application"
+    $sp = Get-AzADApplication -ObjectId $app.ObjectId | New-AzADServicePrincipal
+
+    $app = Get-AzADApplication -ObjectId $app.ObjectId
+
+    Write-Host "##vso[task.setvariable variable=ObjectId;]$($app.ObjectId)"
+    Write-Host "##vso[task.setvariable variable=ApplicationId;]$($app.ApplicationId)"
+    Write-Host "##vso[task.setvariable variable=Name;]$($app.DisplayName)"
+    Write-Host "##vso[task.setvariable variable=AppIdUri;]$($app.IdentifierUris[0])"
+    Write-Host "##vso[task.setvariable variable=HomePageUrl;]$($app.HomePage)"
+    Write-Host "##vso[task.setvariable variable=ServicePrincipalObjectId;]$($servicePrincipal.Id)"
+
+    $result = [PSCustomObject]@{
+        Application      = $app
+        ServicePrincipal = $sp
+    }
+                    
+    $result
 }
 
 function Get-AadApplication {
