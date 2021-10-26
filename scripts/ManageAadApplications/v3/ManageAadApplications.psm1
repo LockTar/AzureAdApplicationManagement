5bbbb800d780bbeaed75036bb0a9dc304d12a961
Ralph Jansen <ralph.jansen@outlook.com>
2021-10-26 21:22:43 +0200

    Create new applications with the new identifieruri

--- scripts/ManageAadApplications/v3/ManageAadApplications.psm1
+++ scripts/ManageAadApplications/v3/ManageAadApplications.psm1
@@ -9,31 +9,21 @@ function New-AadApplication {
         [string]$IdentifierUri
     )
 
-    Write-Verbose "Create new application $DisplayName"
-        
-    $identifierUriFromParameter = $false
     Write-Verbose "Check if IdentifierUri is given as parameter"
     if ([string]::IsNullOrWhiteSpace($IdentifierUri)) {
-        Write-Verbose "No IdentifierUri so generate one with format: https://{DisplayName}"        
-        $IdentifierUri = "https://$DisplayName"
-        $identifierUriFromParameter = $false
-        Write-Verbose "Generated IdentifierUri: $IdentifierUri"
-    }
-    else {
-        Write-Verbose "Use given IdentifierUri: $IdentifierUri"
-        $identifierUriFromParameter = $true
-    }
+        Write-Verbose "Create application $DisplayName"
+        $app = New-AzADApplication -DisplayName $DisplayName
 
-    Write-Verbose "Create application $DisplayName"
-    $app = New-AzADApplication -DisplayName $DisplayName -IdentifierUris $IdentifierUri
-    
-    if ($identifierUriFromParameter -eq $false) {
         # The IdentifierUri was not given as parameter so create the new default identifieruri. This can only be done with the ApplicationId so use it from the 'New' action.
         Write-Verbose "Change IdentifierUri to the new default format of Microsoft: api://{ApplicationId}"
         $IdentifierUri = "api://$($app.ApplicationId)"
         $app = Update-AzADApplication -ObjectId $app.ObjectId -IdentifierUris $IdentifierUri
     }
-
+    else {
+        Write-Verbose "Create application $DisplayName with given IdentifierUri: $IdentifierUri"
+        $app = New-AzADApplication -DisplayName $DisplayName -IdentifierUri $IdentifierUri
+    }
+    
     Write-Verbose "Create service principal connected to application"
     $sp = Get-AzADApplication -ObjectId $app.ObjectId | New-AzADServicePrincipal
 
