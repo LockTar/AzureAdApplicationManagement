5e416d7ce1afeabd43687346ec64f86821d6d978
Ralph Jansen <ralph.jansen@outlook.com>
2023-04-07 12:19:45 +0200

    Create v4 version of the other tasks

---
+++ Vsts-Extension/Get-AzureAdApplication/Get-AzureAdApplicationv4/Main.ps1
@@ -0,0 +1,60 @@
+Trace-VstsEnteringInvocation $MyInvocation
+
+# Get inputs.
+$method = Get-VstsInput -Name method
+$objectId = Get-VstsInput -Name objectId
+$applicationId = Get-VstsInput -Name applicationId
+$name = Get-VstsInput -Name name
+$failIfNotFound = Get-VstsInput -Name failIfNotFound -AsBool
+
+
+# Cleanup hosted agent with AzureRM modules
+# . "$PSScriptRoot\Utility.ps1"
+# CleanUp-PSModulePathForHostedAgent
+
+# Initialize Azure helpers
+Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
+Import-Module $PSScriptRoot\ps_modules\CustomAzureDevOpsAzureHelpers\CustomAzureDevOpsAzureHelpers.psm1
+
+try 
+{
+    # Login
+    # Initialize-PackageProvider
+    # Initialize-Module -Name "Az.Accounts" -RequiredVersion "2.6.0"
+    # Initialize-Module -Name "Az.Resources" -RequiredVersion "4.4.0"
+
+    # $connectedServiceName = Get-VstsInput -Name ConnectedServiceNameARM -Require
+    # $endpoint = Get-VstsEndpoint -Name $connectedServiceName -Require
+    # Initialize-AzModule -Endpoint $endpoint
+
+    Initialize-MsGraph
+    
+    Write-Verbose "Input variables are: "
+    Write-Verbose "method: $method"
+    Write-Verbose "objectId: $objectId"
+    Write-Verbose "applicationId: $applicationId"
+    Write-Verbose "name: $name"
+    Write-Verbose "failIfNotFound: $failIfNotFound"
+
+    Import-Module $PSScriptRoot\scripts\ManageAadApplications.psm1
+
+    switch ($method) {
+        "objectId" { 
+            Get-AadApplication -ObjectId $objectId -FailIfNotFound:$failIfNotFound
+        }
+        "applicationid" { 
+            Get-AadApplication -ApplicationId $applicationId -FailIfNotFound:$failIfNotFound
+        }    
+        "name"
+        {
+            Get-AadApplication -DisplayName $name -FailIfNotFound:$failIfNotFound
+        }    
+        default {
+            Write-Error "Unknow method '$method'"
+        }
+    }
+}
+finally {
+    Remove-EndpointSecrets
+    Disconnect-AzureAndClearContext -ErrorAction SilentlyContinue
+}
---
+++ Vsts-Extension/Update-AzureAdApplication/Update-AzureAdApplicationv4/Main.ps1
@@ -0,0 +1,55 @@
+Trace-VstsEnteringInvocation $MyInvocation
+
+# Get inputs.
+$objectId = Get-VstsInput -Name objectId -Require
+$resourceAccessFilePath = Get-VstsInput -Name resourceAccessFilePath
+$appRolesFilePath = Get-VstsInput -Name appRolesFilePath
+
+# Cleanup hosted agent with AzureRM modules
+# . "$PSScriptRoot\Utility.ps1"
+# CleanUp-PSModulePathForHostedAgent
+
+# Initialize Azure helpers
+Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
+Import-Module $PSScriptRoot\ps_modules\CustomAzureDevOpsAzureHelpers\CustomAzureDevOpsAzureHelpers.psm1
+
+try {
+    # Login
+    # Initialize-PackageProvider
+    # Initialize-Module -Name "Az.Accounts" -RequiredVersion "2.6.0"
+    # Initialize-Module -Name "Az.Resources" -RequiredVersion "4.4.0"
+    
+    # $connectedServiceName = Get-VstsInput -Name ConnectedServiceNameARM -Require
+    # $endpoint = Get-VstsEndpoint -Name $connectedServiceName -Require
+    # Initialize-AzModule -Endpoint $endpoint
+
+    # # Login into old AzureAD because Az still doesn't have all the functions
+    # Initialize-Module -Name "AzureAD" -RequiredVersion "2.0.2.140"
+    # Initialize-AzureAD
+
+    Initialize-MsGraph
+
+    Write-Verbose "Input variables are: "
+    Write-Verbose "objectId: $objectId"
+    Write-Verbose "resourceAccessFilePath: $resourceAccessFilePath"
+    Write-Verbose "appRolesFilePath: $appRolesFilePath"
+
+    Import-Module $PSScriptRoot\scripts\ManageAadApplications.psm1
+
+    if ([string]::IsNullOrWhiteSpace($resourceAccessFilePath) -and ![string]::IsNullOrWhiteSpace($appRolesFilePath)) {
+        Update-AadApplication -ObjectId $objectId -AppRolesFilePath $appRolesFilePath
+    }
+    elseif (![string]::IsNullOrWhiteSpace($resourceAccessFilePath) -and [string]::IsNullOrWhiteSpace($appRolesFilePath)) {
+        Update-AadApplication -ObjectId $objectId -ResourceAccessFilePath $resourceAccessFilePath
+    }
+    elseif (![string]::IsNullOrWhiteSpace($resourceAccessFilePath) -and ![string]::IsNullOrWhiteSpace($appRolesFilePath)) {
+        Update-AadApplication -ObjectId $objectId -ResourceAccessFilePath $resourceAccessFilePath -AppRolesFilePath $appRolesFilePath
+    }
+    else {
+        throw "No parameters found"
+    }
+}
+finally {
+    Remove-EndpointSecrets
+    Disconnect-AzureAndClearContext -ErrorAction SilentlyContinue
+}
