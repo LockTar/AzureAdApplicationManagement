dfcf86a80d38936d95b7b26566da9ae24de22b5e
Ralph Jansen <ralph.jansen@outlook.com>
2023-04-07 11:57:47 +0200

    Fix gulp task to look for v4 folders

--- Vsts-Extension/gulp-tasks/v4/removeAzureAdApplication.js
+++ Vsts-Extension/gulp-tasks/v4/removeAzureAdApplication.js
@@ -5,28 +5,28 @@ var psModulesFolderName = 'ps_modules'
 
 var paths = {
   extension : {
-    psModules : '../scripts/Common/v3/',
+    psModules : '../scripts/Common/v4/',
     removeAzureAdApplication : {
-      v3 : 'Remove-AzureAdApplication/Remove-AzureAdApplicationV3/'
+      v4 : 'Remove-AzureAdApplication/Remove-AzureAdApplicationv4/'
     }
   },
   code : {
     root : '../',
     scripts : '../scripts/',
     manageAadApplications : {
-      v3 : '../scripts/ManageAadApplications/v3/'
+      v4 : '../scripts/ManageAadApplications/v4/'
     },
     vstsAzureHelpers : '../scripts/VstsAzureHelpers/'
   }
 }
 
 function cleanRemoveAzureAdApplication() {
-  console.log('Delete everything in ' + paths.extension.removeAzureAdApplication.v3);
+  console.log('Delete everything in ' + paths.extension.removeAzureAdApplication.v4);
   return del([
-    paths.extension.removeAzureAdApplication.v3 + 'scripts',
-    paths.extension.removeAzureAdApplication.v3 + 'CoreAz.ps1',
-    paths.extension.removeAzureAdApplication.v3 + 'Utility.ps1',
-    paths.extension.removeAzureAdApplication.v3 + psModulesFolderName
+    paths.extension.removeAzureAdApplication.v4 + 'scripts',
+    paths.extension.removeAzureAdApplication.v4 + 'CoreAz.ps1',
+    paths.extension.removeAzureAdApplication.v4 + 'Utility.ps1',
+    paths.extension.removeAzureAdApplication.v4 + psModulesFolderName
   ]);
 }
 
@@ -34,30 +34,30 @@ function buildPsModulesRemoveAzureAdApplication() {
   console.log('Fill the ps modules');
   
   gulp.src(paths.code.scripts + 'CustomAzureDevOpsAzureHelpers/**/*')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + psModulesFolderName + "/CustomAzureDevOpsAzureHelpers"));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + psModulesFolderName + "/CustomAzureDevOpsAzureHelpers"));
 
   gulp.src(paths.extension.psModules + 'TlsHelper_/**/*')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + psModulesFolderName + "/TlsHelper_"));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + psModulesFolderName + "/TlsHelper_"));
 
   gulp.src(paths.extension.psModules + 'VstsAzureHelpers_/**/*')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + psModulesFolderName + "/VstsAzureHelpers_"));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + psModulesFolderName + "/VstsAzureHelpers_"));
 
   gulp.src(paths.extension.psModules + 'VstsAzureRestHelpers_/**/*')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + psModulesFolderName + "/VstsAzureRestHelpers_"));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + psModulesFolderName + "/VstsAzureRestHelpers_"));
 
   return gulp.src(paths.extension.psModules + 'VstsTaskSdk/**/*')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + psModulesFolderName + "/VstsTaskSdk"));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + psModulesFolderName + "/VstsTaskSdk"));
 }
 
 function buildScriptFilesAzureADApplication() {
   gulp.src(paths.code.scripts + 'CoreAz.ps1')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4));
   gulp.src(paths.code.scripts + 'Utility.ps1')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3));
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4));
 
-  console.log('Fill ' + paths.extension.removeAzureAdApplication.v3 + ' scripts from ' + paths.code.manageAadApplications.v3);
-  return gulp.src(paths.code.manageAadApplications.v3 + 'ManageAadApplications.psm1')
-    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v3 + 'scripts'));
+  console.log('Fill ' + paths.extension.removeAzureAdApplication.v4 + ' scripts from ' + paths.code.manageAadApplications.v4);
+  return gulp.src(paths.code.manageAadApplications.v4 + 'ManageAadApplications.psm1')
+    .pipe(gulp.dest(paths.extension.removeAzureAdApplication.v4 + 'scripts'));
 }
 
 var taskName = "RemoveAdApplication";
--- Vsts-Extension/gulp-tasks/v4/updateAzureAdApplication.js
+++ Vsts-Extension/gulp-tasks/v4/updateAzureAdApplication.js
@@ -5,28 +5,28 @@ var psModulesFolderName = 'ps_modules'
 
 var paths = {
   extension : {
-    psModules : '../scripts/Common/v3/',
+    psModules : '../scripts/Common/v4/',
     updateAzureAdApplication : {
-      v3 : 'Update-AzureAdApplication/Update-AzureAdApplicationV3/'
+      v4 : 'Update-AzureAdApplication/Update-AzureAdApplicationv4/'
     }
   },
   code : {
     root : '../',
     scripts : '../scripts/',
     manageAadApplications : {
-      v3 : '../scripts/ManageAadApplications/v3/'
+      v4 : '../scripts/ManageAadApplications/v4/'
     },
     vstsAzureHelpers : '../scripts/VstsAzureHelpers/'
   }
 }
 
 function cleanUpdateAzureAdApplication() {
-  console.log('Delete everything in ' + paths.extension.updateAzureAdApplication.v3);
+  console.log('Delete everything in ' + paths.extension.updateAzureAdApplication.v4);
   return del([
-    paths.extension.updateAzureAdApplication.v3 + 'scripts',
-    paths.extension.updateAzureAdApplication.v3 + 'CoreAz.ps1',
-    paths.extension.updateAzureAdApplication.v3 + 'Utility.ps1',
-    paths.extension.updateAzureAdApplication.v3 + psModulesFolderName
+    paths.extension.updateAzureAdApplication.v4 + 'scripts',
+    paths.extension.updateAzureAdApplication.v4 + 'CoreAz.ps1',
+    paths.extension.updateAzureAdApplication.v4 + 'Utility.ps1',
+    paths.extension.updateAzureAdApplication.v4 + psModulesFolderName
   ]);
 }
 
@@ -34,30 +34,30 @@ function buildPsModulesUpdateAzureAdApplication() {
   console.log('Fill the ps modules');
   
   gulp.src(paths.code.scripts + 'CustomAzureDevOpsAzureHelpers/**/*')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + psModulesFolderName + "/CustomAzureDevOpsAzureHelpers"));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + psModulesFolderName + "/CustomAzureDevOpsAzureHelpers"));
 
   gulp.src(paths.extension.psModules + 'TlsHelper_/**/*')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + psModulesFolderName + "/TlsHelper_"));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + psModulesFolderName + "/TlsHelper_"));
 
   gulp.src(paths.extension.psModules + 'VstsAzureHelpers_/**/*')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + psModulesFolderName + "/VstsAzureHelpers_"));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + psModulesFolderName + "/VstsAzureHelpers_"));
 
   gulp.src(paths.extension.psModules + 'VstsAzureRestHelpers_/**/*')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + psModulesFolderName + "/VstsAzureRestHelpers_"));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + psModulesFolderName + "/VstsAzureRestHelpers_"));
 
   return gulp.src(paths.extension.psModules + 'VstsTaskSdk/**/*')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + psModulesFolderName + "/VstsTaskSdk"));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + psModulesFolderName + "/VstsTaskSdk"));
 }
 
 function buildScriptFilesAzureADApplication() {
   gulp.src(paths.code.scripts + 'CoreAz.ps1')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4));
   gulp.src(paths.code.scripts + 'Utility.ps1')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3));
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4));
 
-  console.log('Fill ' + paths.extension.updateAzureAdApplication.v3 + ' scripts from ' + paths.code.manageAadApplications.v3);
-  return gulp.src(paths.code.manageAadApplications.v3 + 'ManageAadApplications.psm1')
-    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v3 + 'scripts'));
+  console.log('Fill ' + paths.extension.updateAzureAdApplication.v4 + ' scripts from ' + paths.code.manageAadApplications.v4);
+  return gulp.src(paths.code.manageAadApplications.v4 + 'ManageAadApplications.psm1')
+    .pipe(gulp.dest(paths.extension.updateAzureAdApplication.v4 + 'scripts'));
 }
 
 var taskName = "UpdateAdApplication";
